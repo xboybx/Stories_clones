165d459d2618f880c3c477fadd6444baaf398e68
xboybx <146360441+xboybx@users.noreply.github.com>
2023-10-28 12:10:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+
+let arr = [
+    {
+        profilepic: "2.jpg"
+        , story: "https://img.freepik.com/premium-vector/young-man-portrait-with-tropical-leaves-background-trendy-hipster-guy-with-earring_662937-243.jpg?w=740"
+
+    }, {
+        profilepic: "3.jpg"
+        , story: "https://img.freepik.com/free-vector/abstract-hand-drawn-woman-portrait_23-2148901737.jpg?w=740&t=st=1697650928~exp=1697651528~hmac=a88db30766229aa621c1b097a77ca69c6f45b9f4d5066b6dd56652b0c30e9560"
+
+    }, {
+        profilepic: "9.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-paisa-character-illustration_23-2150681681.jpg?w=740&t=st=1697651008~exp=1697651608~hmac=1415d91d4b059af5a288905e9b70637ca80912acd75e494694e6a540e6151746"
+    }, {
+        profilepic: "8.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-fingers-crossed-illustration_23-2150185034.jpg?w=740&t=st=1697650578~exp=1697651178~hmac=433510359cf016945ffa35824b8995ad0e307c1089bcaa4d8c298daa605328b8"
+    }, {
+        profilepic: "6.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-international-women-s-day-illustration_23-2149264047.jpg?w=740&t=st=1697651169~exp=1697651769~hmac=c85fbb8673e506ce113e1d9a7829ed786182422c9a4c5468aaae2f6e20dec2f1"
+    }, {
+        profilepic: "7.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-janmashtami-illustration-with-krishna-flute_23-2149481622.jpg?w=740&t=st=1697650976~exp=1697651576~hmac=53be065119e5d656ddf9f90fd8e2db8b2d5afb99734fc074163e10229f5c197b"
+    }, {
+        profilepic: "2.jpg"
+        , story: "https://img.freepik.com/premium-vector/young-man-portrait-with-tropical-leaves-background-trendy-hipster-guy-with-earring_662937-243.jpg?w=740"
+    }, {
+        profilepic: "3.jpg"
+        , story: "https://img.freepik.com/free-vector/abstract-hand-drawn-woman-portrait_23-2148901737.jpg?w=740&t=st=1697650928~exp=1697651528~hmac=a88db30766229aa621c1b097a77ca69c6f45b9f4d5066b6dd56652b0c30e9560"
+    }, {
+        profilepic: "9.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-paisa-character-illustration_23-2150681681.jpg?w=740&t=st=1697651008~exp=1697651608~hmac=1415d91d4b059af5a288905e9b70637ca80912acd75e494694e6a540e6151746"
+    }, {
+        profilepic: "8.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-fingers-crossed-illustration_23-2150185034.jpg?w=740&t=st=1697650578~exp=1697651178~hmac=433510359cf016945ffa35824b8995ad0e307c1089bcaa4d8c298daa605328b8"
+    }, {
+        profilepic: "6.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-international-women-s-day-illustration_23-2149264047.jpg?w=740&t=st=1697651169~exp=1697651769~hmac=c85fbb8673e506ce113e1d9a7829ed786182422c9a4c5468aaae2f6e20dec2f1"
+    }, {
+        profilepic: "7.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-janmashtami-illustration-with-krishna-flute_23-2149481622.jpg?w=740&t=st=1697650976~exp=1697651576~hmac=53be065119e5d656ddf9f90fd8e2db8b2d5afb99734fc074163e10229f5c197b"
+    }, {
+        profilepic: "2.jpg"
+        , story: "https://img.freepik.com/premium-vector/young-man-portrait-with-tropical-leaves-background-trendy-hipster-guy-with-earring_662937-243.jpg?w=740"
+    }, {
+        profilepic: "3.jpg"
+        , story: "https://img.freepik.com/free-vector/abstract-hand-drawn-woman-portrait_23-2148901737.jpg?w=740&t=st=1697650928~exp=1697651528~hmac=a88db30766229aa621c1b097a77ca69c6f45b9f4d5066b6dd56652b0c30e9560"
+    }, {
+        profilepic: "9.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-paisa-character-illustration_23-2150681681.jpg?w=740&t=st=1697651008~exp=1697651608~hmac=1415d91d4b059af5a288905e9b70637ca80912acd75e494694e6a540e6151746"
+    }, {
+        profilepic: "8.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-fingers-crossed-illustration_23-2150185034.jpg?w=740&t=st=1697650578~exp=1697651178~hmac=433510359cf016945ffa35824b8995ad0e307c1089bcaa4d8c298daa605328b8"
+    }, {
+        profilepic: "6.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-international-women-s-day-illustration_23-2149264047.jpg?w=740&t=st=1697651169~exp=1697651769~hmac=c85fbb8673e506ce113e1d9a7829ed786182422c9a4c5468aaae2f6e20dec2f1"
+    }, {
+        profilepic: "7.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-janmashtami-illustration-with-krishna-flute_23-2149481622.jpg?w=740&t=st=1697650976~exp=1697651576~hmac=53be065119e5d656ddf9f90fd8e2db8b2d5afb99734fc074163e10229f5c197b"
+    }, {
+        profilepic: "2.jpg"
+        , story: "https://img.freepik.com/premium-vector/young-man-portrait-with-tropical-leaves-background-trendy-hipster-guy-with-earring_662937-243.jpg?w=740"
+    }, {
+        profilepic: "3.jpg"
+        , story: "https://img.freepik.com/free-vector/abstract-hand-drawn-woman-portrait_23-2148901737.jpg?w=740&t=st=1697650928~exp=1697651528~hmac=a88db30766229aa621c1b097a77ca69c6f45b9f4d5066b6dd56652b0c30e9560"
+    }, {
+        profilepic: "9.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-paisa-character-illustration_23-2150681681.jpg?w=740&t=st=1697651008~exp=1697651608~hmac=1415d91d4b059af5a288905e9b70637ca80912acd75e494694e6a540e6151746"
+    }, {
+        profilepic: "8.jpg"
+        , story: "https://img.freepik.com/free-vector/hand-drawn-fingers-crossed-illustration_23-2150185034.jpg?w=740&t=st=1697650578~exp=1697651178~hmac=433510359cf016945ffa35824b8995ad0e307c1089bcaa4d8c298daa605328b8"
+    }, {
+        profilepic: "6.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-international-women-s-day-illustration_23-2149264047.jpg?w=740&t=st=1697651169~exp=1697651769~hmac=c85fbb8673e506ce113e1d9a7829ed786182422c9a4c5468aaae2f6e20dec2f1"
+    }, {
+        profilepic: "7.jpg"
+        , story: "https://img.freepik.com/free-vector/flat-janmashtami-illustration-with-krishna-flute_23-2149481622.jpg?w=740&t=st=1697650976~exp=1697651576~hmac=53be065119e5d656ddf9f90fd8e2db8b2d5afb99734fc074163e10229f5c197b"
+    },
+
+]
+let arr2 = [{ post: "https://img.freepik.com/free-vector/flat-design-man-forest-illustration_52683-94451.jpg?w=740&t=st=1697655432~exp=1697656032~hmac=1ead6f6dc8a006ec4967cdcefe100cebdb41b13fc541e4cce91c5973f976b53b" }, { post: "https://img.freepik.com/free-vector/flat-adventure-background_23-2149031058.jpg?w=1060&t=st=1697655464~exp=1697656064~hmac=894e81f861803b247ed9516679c2fc53c688540897af06d6d1348c5f0a5b744e" }, { post: "https://img.freepik.com/free-vector/flat-design-adventure-background_23-2149031059.jpg?w=1060&t=st=1697655484~exp=1697656084~hmac=c9375b036ae6912a819338a87725e41925eaf322732ed9ec2b0da0337534f8f4" }, { post: "https://img.freepik.com/free-vector/hand-drawn-indian-family-illustration_52683-85741.jpg?w=826&t=st=1697655520~exp=1697656120~hmac=21e58f9affc32a6468b2cba7c5a506d8937b32f2f9323492e74b63446e3d20d3" }]
+
+
+let clutter = ""
+// looping through aray and adding object's profile into html
+arr.forEach((elem, i) => {
+    //    console.log(i,elem)
+    clutter +=
+        `<div class="story">
+   <img  id="${i}" src="${elem.profilepic}" alt="story">
+        </div>`
+})
+
+document.querySelector(".storiesHolder").innerHTML = clutter;
+
+let storyholder = document.querySelector(".storiesHolder");
+let StoryFullscreen = document.querySelector(".storyfullscreen")
+
+storyholder.addEventListener("click", (details) => {
+
+    let storyviewer = arr[details.target.id].story;
+
+    StoryFullscreen.style.display = "block"
+    StoryFullscreen.style.backgroundImage = `url("${storyviewer}")`
+
+    setTimeout(() => {
+        document.querySelector(".storyfullscreen").style.display = "none"
+
+    }, 1000)
+})
+
+
+let clutter2 = ""
+
+
+arr2.forEach((elem2, indx) => {
+    //    console.log(i,elem)
+    clutter2 +=
+        `  <div class="posts">
+        <img id="${indx}" src="${elem2.post}" alt="post">
+        <i class="ri-heart-3-fill" id="js-heart"></i>
+    </div>
+    `
+})
+
+document.querySelector(".allposts").innerHTML = clutter2;
+
+
+let like = document.querySelector("#js-heart")
+let posts = document.querySelector(".posts")
+
+posts.addEventListener('dblclick', () => {
+    like.style.position = "absolute"
+    like.style.top = "50%"
+    like.style.left = "50%"
+    like.style.transform = "translate(-50%, -50%) Scale(0)"
+    like.style.fontSize = "2.8rem"
+    like.style.color = "white"
+    like.style.transform = "translate(-50%, -50%) Scale(1)"
+    like.style.opacity = "0.80"
+    like.style.transition = "0.50s"
+
+
+    setTimeout(function () {
+        like.style.transform = "translate(-50%, -50%) Scale(0)"
+        like.style.transition = "none"
+    }, 1500)
+});
+
+
+
+
+
